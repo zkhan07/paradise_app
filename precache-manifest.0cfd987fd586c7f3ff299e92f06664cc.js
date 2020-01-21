6a4a86c9918270d476a5f503cb61586c0bc98778
zaid <khanzaid786786.kz@gmail.com>
2020-01-21 11:21:33 +0530

    Updates

---
+++ precache-manifest.0cfd987fd586c7f3ff299e92f06664cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d5110423c05b2225e7aede50a743ab8",
+    "url": "/paradise_app/index.html"
+  },
+  {
+    "revision": "c9e4f834e723f31bf01b",
+    "url": "/paradise_app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6018a0d94f7a492bc210",
+    "url": "/paradise_app/static/js/2.11a68398.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/paradise_app/static/js/2.11a68398.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c9e4f834e723f31bf01b",
+    "url": "/paradise_app/static/js/main.edd0be07.chunk.js"
+  },
+  {
+    "revision": "4c34b5c0ef9e60d5b985",
+    "url": "/paradise_app/static/js/runtime-main.37084dc3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/paradise_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
